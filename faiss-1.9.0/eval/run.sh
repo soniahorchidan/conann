0f149956c1cc9b2f36c7105edad755151d36d022
soniahorchidan <soniahorchidan@gmail.com>
2025-01-23 14:31:00 +0100

    Add latency measurements faiss

--- faiss-1.9.0/eval/run.sh
+++ faiss-1.9.0/eval/run.sh
@@ -4,7 +4,9 @@
 
 # ./build/eval/error bert 500 500 100 0.4 > "../Faiss_efficiency_bert_100_0.4.log"
 
-./build/eval/error bert_10 0.8 0.6
+# ./build/eval/error bert_10 0.8 0.6
+
+./build/eval/latency bert_10 0.5 0.1
 
 # ./build/eval/bound bert 500 500 100 0.1
 
