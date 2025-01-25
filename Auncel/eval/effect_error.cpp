37cd60a7d2d8da34ba70c2f37f7a1bc6e2ef8c2d
Fabian Zeiher <fzeiher@gmail.com>
2025-01-25 23:29:58 +0100

    attempted bugfix for auncel

--- Auncel/eval/effect_error.cpp
+++ Auncel/eval/effect_error.cpp
@@ -375,6 +375,8 @@ int main(int argc, char **argv) {
     nq = (nq / 10) * 10;
     // get training and testing sizes
     int ts = nq * training_fraction;
+    // Round down to the nearest number divisible by ten (necessary for auncel, don't know why)
+    ts = (ts / 10) * 10;
     int ses = nq - ts;
     // Run error profile system
     {
