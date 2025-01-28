e51c30d6b06ab7dc20302ce256a7fa07410d33ac
soniahorchidan <soniahorchidan@gmail.com>
2025-01-28 15:40:23 +0000

    Fix

--- faiss-1.9.0/eval/run.sh
+++ faiss-1.9.0/eval/run.sh
@@ -3,9 +3,9 @@
 # Error validity + efficiency
 ./build/eval/error bert_100 0.5 0.2 0.1 0.05
 ./build/eval/error glove_100 0.5 0.2 0.1 0.05
-/build/eval/error gist 0.5 0.2 0.1 0.05
-/build/eval/error deep10M 0.5 0.2 0.1 0.05
-/build/eval/error sift10M 0.5 0.2 0.1 0.05
+./build/eval/error gist 0.5 0.2 0.1 0.05
+# ./build/eval/error deep10M 0.5 0.2 0.1 0.05
+# ./build/eval/error sift10M 0.5 0.2 0.1 0.05
 
 # Variable k
 ./build/eval/error bert_10 0.5 0.1
